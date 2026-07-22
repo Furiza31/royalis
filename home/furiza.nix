24a6f8a36d7e6b73a3ec10b2109dc29b91b3feba
Furiza31 <hugo.wendjaneh@gmail.com>
2026-07-22 23:24:13 +0200

    feat: tinty

--- home/furiza.nix
+++ home/furiza.nix
@@ -1,7 +1,9 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 let
   dotfiles = "${config.home.homeDirectory}/nixos-config/dotfiles";
+  tintyData = "${config.xdg.dataHome}/tinted-theming/tinty";
+  tintyConfig = "${config.xdg.configHome}/tinted-theming/tinty/config.toml";
 in
 {
   home.username = "furiza";
@@ -15,6 +17,7 @@ in
     vscode
     codex
     quickshell
+    tinty
   ];
 
   xdg.configFile."kitty".source =
@@ -26,8 +29,33 @@ in
   xdg.configFile."quickshell".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/quickshell";
 
+  xdg.configFile."tinted-theming/tinty".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/tinted-theming/tinty";
+
   home.file.".zshrc".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.zshrc";
+
+  home.activation.tintyCatppuccin = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    tinty_data="${tintyData}"
+    tinty_config="${tintyConfig}"
+    tinty_repos="$tinty_data/repos"
+    tinty_schemes="$tinty_repos/schemes"
+    kitty_template="$tinty_repos/kitty"
+    niri_template="$tinty_repos/niri"
+
+    mkdir -p "$tinty_schemes/base24" "$tinty_repos"
+    cp -f "${pkgs.base24-schemes}/share/themes/catppuccin-latte.yaml" "$tinty_schemes/base24/catppuccin-latte.yaml"
+    cp -f "${pkgs.base24-schemes}/share/themes/catppuccin-frappe.yaml" "$tinty_schemes/base24/catppuccin-frappe.yaml"
+
+    rm -rf "$kitty_template" "$niri_template"
+    cp -R "${dotfiles}/tinted-theming/tinty/templates/kitty" "$kitty_template"
+    cp -R "${dotfiles}/tinted-theming/tinty/templates/niri" "$niri_template"
+    chmod -R u+w "$kitty_template" "$niri_template"
+
+    ${pkgs.tinty}/bin/tinty -c "$tinty_config" -d "$tinty_data" build -q "$kitty_template"
+    ${pkgs.tinty}/bin/tinty -c "$tinty_config" -d "$tinty_data" build -q "$niri_template"
+    ${pkgs.tinty}/bin/tinty -c "$tinty_config" -d "$tinty_data" apply -q base24-catppuccin-latte
+  '';
 
   programs.home-manager.enable = true;
 }
